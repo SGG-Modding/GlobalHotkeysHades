0e620a0a831cc8eb1f38f487e4f469f140751388
SolidplasmaStudios <55113422+SolidplasmaStudios@users.noreply.github.com>
2022-06-16 23:01:11 -0400

    Add files via upload
    Updated the lua file: spawn_enemies to spawn 1-5 random enemies from the game's enemy pool, disabled PureDamage parameter for damaging effects, which prevents the player from taking damage in the house.

--- GlobalHotkeysHades/GlobalHotkeysHades.lua
+++ GlobalHotkeysHades/GlobalHotkeysHades.lua
@@ -2,24 +2,32 @@ ModUtil.Mod.Register( "GlobalHotkeysHades" )
 
 GlobalHotkeysHades.Events = {
   	one_health = function( )
-		Damage( CurrentRun.Hero, { triggeredById = CurrentRun.Hero.ObjectId, DamageAmount = CurrentRun.Hero.MaxHealth - 1, MinHealth = 1, PureDamage = true, Silent = false })
+		Damage( CurrentRun.Hero, { triggeredById = CurrentRun.Hero.ObjectId, DamageAmount = CurrentRun.Hero.MaxHealth - 1, MinHealth = 1, PureDamage = false, Silent = false })
   	end,
 	fifty_damage = function( )
-		Damage( CurrentRun.Hero, { triggeredById = CurrentRun.Hero.ObjectId, DamageAmount = CurrentRun.Hero.MaxHealth / 2, MinHealth = 1, PureDamage = true, Silent = false })
+		Damage( CurrentRun.Hero, { triggeredById = CurrentRun.Hero.ObjectId, DamageAmount = 50, MinHealth = 0, PureDamage = false, Silent = false })
 	end,
 	instant_death = function( )
 		KillHero({},{},{})
 	end,
 	spawn_enemies = function( )
 		local EnemyTable = {}
-		local amount = math.random(1,8)
+		local   EnemyNames =
+		{
+		  "IllusionistClone", "ShieldRangedElite", "BlastCubeFusedRegenerating", "PunchingBagUnitSuperElite", "LightSpawner", "SwarmerSuperElite",
+		  "CrusherUnit", "ChariotSuicide", "IllusionistClone", "ShieldRangedElite", "BlastCubeFusedRegenerating", "PunchingBagUnitSuperElite", "ReflectiveMirror", "LightSpawner",
+		  "SwarmerSuperElite", "CrusherUnit", "ChariotSuicide", "ShadeSpawnerElite", "UnstableGenerator", "HeavyMeleeSuperElite", "HeavyRangedElite",
+		  "ShadeBowUnitSuperElite", "TimeCrystal", "Illusionist", "GunBombUnit", "HeavyRanged", "FlurrySpawner", "RangedBurrowerElite", "SplitShotUnitElite", "Harpy",
+		  "HeavyRangedSplitterMinibossSuperElite", "ChariotSuperElite", "SupportShields", "BloodlessNakedBerserkerElite", "ShadeNaked", "ShadeSwordUnit", "BloodlessSelfDestructElite", "CrusherUnitSuperElite", "LightRanged", "HeavyMelee", "HealRanged", "ThiefMineLayer"
+		}
+		local amount = math.random(1,5)
 		for i = 1, amount do
-		  table.insert(EnemyTable,"ChariotSuicide")
+		  table.insert(EnemyTable,EnemyNames[math.random(44)])
 		end
 		for _,v in ipairs(EnemyTable) do
 		  local enemyData = EnemyData[v]
 		  local newEnemy = DeepCopyTable( enemyData )
-		  newEnemy.ObjectId = SpawnUnit({ Name = newEnemy.Name, Group = "Standing", DestinationId = CurrentRun.Hero.ObjectId, OffsetX = math.random(-500,500), OffsetY = math.random(-500,500) })
+		  newEnemy.ObjectId = SpawnUnit({ Name = enemyData.Name, Group = "Standing", DestinationId = CurrentRun.Hero.ObjectId, OffsetX = math.random(-500,500), OffsetY = math.random(-500,500) })
 		  SetupEnemyObject( newEnemy, CurrentRun, { SkipSpawnVoiceLines = true } )
 		  UseableOff({ Id = newEnemy.ObjectId })
 		end
